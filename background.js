bd186846e76ff86ea883bf937b6f5dbdb1cbb841
nezorepla <alperozen84@gmail.com>
2023-05-22 10:43:32 +0300

    Add files via upload

---
+++ background.js
@@ -0,0 +1,18 @@
+// Sağ tıklama menüsüne özel işlevi ekle
+chrome.contextMenus.create({
+  id: "vkSearch",
+  title: "VK'da Ara",
+  contexts: ["selection"]
+
+});
+ //    var searchUrl = 'https://www.google.com/search?q=' + searchString;
+// Sağ tıklama menüsü işlevi gerçekleştirildiğinde tetiklenir
+chrome.contextMenus.onClicked.addListener(function(info, tab) {
+  if (info.menuItemId === "vkSearch") {
+    var selectedText = info.selectionText.trim();
+    var searchString = selectedText + ' site:vk.com/video';
+  var searchUrl = 'https://www.google.com/search?q=' + encodeURIComponent(searchString);
+  //chrome.tabs.create({ url: searchUrl });
+  window.open(searchUrl, '_blank');
+  }
+});
